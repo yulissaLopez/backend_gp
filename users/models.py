bb498f6169262745629871d5a09c2403cdebc92f
yulissaLopez <yulissa.lopezcortes@hotmail.com>
2024-10-29 10:56:54 -0500

    creacion de modelo user _ NO FINISHED

--- users/models.py
+++ users/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+import uuid
 
 # Create your models here.
+class User(AbstractUser):
+    # id con uuid
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    username = models.CharField(max_length=150, unique=True)
+    email = models.EmailField(unique=True)
+
+    
