8c4df86a976d68b820d8996aba3b121d931ca08a
Yulissa Lopez <lyulissa5@gmail.com>
2024-11-11 17:10:54 -0500

    Cambio de modelo user AbstractUser -> AbstractBaseUser

--- users/models.py
+++ users/models.py
@@ -1,34 +1,27 @@
 from django.db import models
 from .managers import UserManager
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser
 import uuid
 
 # Create your models here.
 # MODELO  
-class CustomUser(AbstractUser):
+class CustomUser(AbstractBaseUser):
 
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    username = None
     name = models.CharField(max_length=100, unique=False)
-
     email = models.EmailField(max_length=100, unique=True)
-
-    created_at = models.DateTimeField(auto_now_add=True)
     is_active = models.BooleanField(default=True)
-    is_admin = models.BooleanField(default= False)
     is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    # TO DO REVISAR ESTE CAMPO PARA VERIFICACION DE USUARIO DE A TRAVES DEL CORREO
-    is_verified = models.BooleanField(default=False)
+    # TODO REVISAR ESTE CAMPO PARA VERIFICACION DE USUARIO DE A TRAVES DEL CORREO
+    # is_verified = models.BooleanField(default=False)
 
-    """indica que utilice el UserManager para interactuar con las instancias del modelo"""
+    # Indica que utilice el UserManager para interactuar con las instancias del modelo
     objects = UserManager()
 
-    """ Le indica a django que se usara el email en lugar del username en la autenticacion"""
+    # Indica a django que se usara el email en lugar del username en la autenticacion
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['name']
 
+    # Al imprimir el objecto se mostrara el valor de name en lugar de la representacion predeterminada del objeto
     def __str__(self) :
         return self.name
-
-#REVISAR EL USO DE BASEABSTRACTUSER
